fa4e933face330a942312f1a0c2dd19af7d630ad
Axel Andersson <axel.andersson@elev.ga.ntig.se>
2023-11-16 22:18:13 +0100

    fsdf

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using Raylib_cs;
+
+Raylib.SetTargetFPS(60);
+
+while (!Raylib.WindowShouldClose())
+{
+  Raylib.BeginDrawing();
+
+  Raylib.ClearBackground(Color.WHITE);
+  
+  Raylib.EndDrawing();
+}
+
+
+
+
+
+
+
+
+
+
+
+
